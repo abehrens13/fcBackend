e8e53d4ee24205d1bf8f23057869fc6e6a31445f
abehrens13 <andrey.behrens@openaqua.de>
2020-05-11 22:32:24 +0200

    init

---
+++ src/main/java/de/openaqua/fcbackend/ScrumtestbackendApplication.java
@@ -0,0 +1,13 @@
+package de.openaqua.fcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumtestbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumtestbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fcbackend/TestQuizz.java
@@ -0,0 +1,19 @@
+package fcbackend;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import de.openaqua.fcbackend.entities.Quizz;
+
+class TestQuizz {
+
+	@Test
+	void test() {
+		Quizz q = new Quizz();
+		assertEquals("", q.getDescription());
+		q.setDescription("abc");
+		assertEquals("abc", q.getDescription());
+	}
+
+}
