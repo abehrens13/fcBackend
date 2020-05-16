77990dc566ffb13691e8902ea91aa93ba8bd2916
abehrens13 <andrey.behrens@openaqua.de>
2020-05-17 01:07:04 +0200

    fix on redis and question file
    fixed redis configuration and question file will now be loaded from jar
    file

--- src/main/java/de/openaqua/fcbackend/repositories/QuestionsRepository.java
+++ src/main/java/de/openaqua/fcbackend/repositories/QuestionsRepository.java
@@ -1,19 +1,18 @@
 package de.openaqua.fcbackend.repositories;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.ResourceUtils;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
 import de.openaqua.fcbackend.entities.Question;
 import de.openaqua.fcbackend.entities.Quizz;
 
@@ -21,7 +20,9 @@ import de.openaqua.fcbackend.entities.Quizz;
 public class QuestionsRepository {
   private static final Logger LOG = LoggerFactory.getLogger(QuestionsRepository.class);
   private Quizz quizz;
-  private static String filename = "questions2.yaml";
+
+  @Value("classpath:/static/questions2.yaml")
+  private Resource questionFile;
 
   public QuestionsRepository() {
     LOG.debug("setup QuestionsRepository()");
@@ -32,20 +33,16 @@ public class QuestionsRepository {
     LOG.info("loadDefaultQuestionFile");
     ObjectMapper om = new ObjectMapper(new YAMLFactory());
     try {
-      File file = ResourceUtils.getFile("classpath:" + filename);
-      quizz = om.readValue(file, Quizz.class);
-      LOG.info("Loaded quizz data from {}", filename);
+      InputStream dbAsStream = questionFile.getInputStream();
+      quizz = om.readValue(dbAsStream, Quizz.class);
+
+      LOG.info("Loaded quizz data from {}", questionFile.getFilename());
 
       Map<String, Question> m = quizz.getQuestions();
       m.forEach((key, value) -> value.setQuestionStr(key));
 
-    } catch (FileNotFoundException e1) {
-      LOG.error("cannot load from file {} cause {}", filename, e1.getLocalizedMessage());
-      LOG.info("Exception", e1);
-
-    } catch (IOException e) {
-      LOG.error("cannot load from file {} cause {}", filename, e.getLocalizedMessage());
-      LOG.info("EXCEPTION:", e);
+    } catch (IOException e1) {
+      LOG.error("cannot load from file {} cause {}", questionFile.getFilename(), e1.getLocalizedMessage());
     }
   }
 
@@ -56,9 +53,7 @@ public class QuestionsRepository {
       om.writeValue(new File(filename), quizz);
     } catch (IOException e) {
       LOG.error("cannot write to file {} cause {}", filename, e.getLocalizedMessage());
-      LOG.info("EXCEPTION:", e);
     }
-
   }
 
   public Optional<Quizz> getAll() {
