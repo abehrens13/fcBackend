bbba94f2c6a16e35558b9c76abd1c45c8eeceb1c
abehrens13 <andrey.behrens@openaqua.de>
2020-05-20 19:22:40 +0200

    fix unit tests

--- src/test/java/de/openaqua/fcbackend/controller/QuizzSessionControllerTest.java
+++ src/test/java/de/openaqua/fcbackend/controller/QuizzSessionControllerTest.java
@@ -1,39 +1,36 @@
 package de.openaqua.fcbackend.controller;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import de.openaqua.fcbackend.model.QuizzSession;
 
 @SpringBootTest()
 class QuizzSessionControllerTest {
-  // @Autowired
-  // private QuizzSessionController controller;
+  @Autowired
+  private QuizzSessionController controller;
 
   @Test
   void testNewSession() {
     assertFalse(1 == 2);
 
     // get new session
-    // ResponseEntity<QuizzSession> r1 = controller.sessionGet();
-    // assertTrue(r1.getStatusCodeValue() == 200);
-    // QuizzSession s = r1.getBody();
-    // assertFalse(s.getId().isEmpty());
+    ResponseEntity<QuizzSession> r1 = controller.getSession();
+    assertTrue(r1.getStatusCodeValue() == 200);
+    QuizzSession s = r1.getBody();
+    assertFalse(s.getId().isEmpty());
 
-    /*
-     * // patch session
-     * ResponseEntity<QuizzSession> r2 = controller.patchSession(s.getId());
-     * assertTrue(r2.getStatusCodeValue() == 200);
-     * assertTrue(r1.getBody().getId().equals(r2.getBody().getId()));
-     * // delete session
-     * ResponseEntity<String> r3 = controller.sessionDelete(s.getId());
-     * assertTrue(r3.getStatusCodeValue() == 200);
-     * assertTrue(r1.getBody().getId().equals(r3.getBody()));
-     * // patch illegal session
-     * Assertions.assertThrows(ResourceAccessException.class, () -> controller.patchSession(s.getId()));
-     * // delete illegal session
-     * Assertions.assertThrows(ResourceAccessException.class, () ->
-     * controller.deleteSession(s.getId()));
-     */
+    // delete session
+    ResponseEntity<Void> r3 = controller.deleteSession(s.getId());
+    assertTrue(r3.getStatusCodeValue() == 200);
+    // delete illegal session
+    ResponseEntity<Void> r4 = controller.deleteSession(s.getId());
+    assertTrue(r4.getStatusCodeValue() == 410);
+
   }
 }
