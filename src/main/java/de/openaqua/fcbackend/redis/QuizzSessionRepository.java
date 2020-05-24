50fb24269cb319caac314dd4daa82b4d54dcdfee
abehrens13 <andrey.behrens@openaqua.de>
2020-05-24 03:37:37 +0200

    todo added

--- src/main/java/de/openaqua/fcbackend/redis/QuizzSessionRepository.java
+++ src/main/java/de/openaqua/fcbackend/redis/QuizzSessionRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import de.openaqua.fcbackend.entities.QuizzSession;
 
+//TODO: Redis forgets values from QuizzSession. Whats going on???
 public interface QuizzSessionRepository extends CrudRepository<QuizzSession, String> {
 }
