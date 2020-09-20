6df8e82ef59e1cd6ee76d47e343c2000769f9f25
abehrens13 <Andrey.Behrens@openaqua.de>
2020-09-20 15:23:13 +0200

    sonar

--- src/test/java/de/openaqua/fcbackend/controller/QuizzSessionControllerTest.java
+++ src/test/java/de/openaqua/fcbackend/controller/QuizzSessionControllerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -23,58 +24,63 @@ import de.openaqua.fcbackend.entities.QuizzSession;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = FcBackendApplication.class)
 class QuizzSessionControllerTest {
 
-  @LocalServerPort
-  int randomServerPort;
+    @LocalServerPort
+    int randomServerPort;
 
-  @Test
-  @Order(1)
-  public void testEnvironment() throws Exception {
-    assertTrue(randomServerPort > 0);
-  }
-
-  private ResponseEntity<QuizzSession> getSession() throws URISyntaxException {
-    RestTemplate restTemplate = new RestTemplate();
-    final String baseUrl = "http://localhost:" + randomServerPort + "/quizz";
-    URI url = new URI(baseUrl);
-    return restTemplate.getForEntity(url, QuizzSession.class);
-  }
-
-  @Test
-  public void testGetSession() throws Exception {
-    ResponseEntity<QuizzSession> result = getSession();
-
-    assertEquals(200, result.getStatusCodeValue());
-    assertNotNull(result.getBody());
-    QuizzSession session = result.getBody();
-
-    assertNotNull(session.getId());
-    assertNotNull(session.getCreationTime());
-    assertTrue(!session.getId().isEmpty());
-  }
-
-  @Test
-  void testDeleteSessionIllegal() throws URISyntaxException {
-    RestTemplate restTemplate = new RestTemplate();
-    final String baseUrl = "http://localhost:" + randomServerPort + "/quizz/abc";
-    URI url = new URI(baseUrl);
-
-    try {
-      restTemplate.delete(url);
-    } catch (RestClientException e) {
-      assertTrue(e.getMessage().startsWith("410"));
+    private String getBaseUrl() {
+        final String url = "http://localhost:";
+        return url + randomServerPort;
     }
-  }
 
-  @Test()
-  void testDeleteSessionLegal() throws URISyntaxException {
-    ResponseEntity<QuizzSession> result = getSession();
-    String session = result.getBody().getId();
+    @Test
+    @Order(1)
+    public void testEnvironment() {
+        assertTrue(randomServerPort > 0);
+    }
 
-    RestTemplate restTemplate = new RestTemplate();
-    final String baseUrl = "http://localhost:" + randomServerPort + "/quizz/" + session;
-    URI url = new URI(baseUrl);
+    private ResponseEntity<QuizzSession> getSession() throws URISyntaxException {
+        RestTemplate restTemplate = new RestTemplate();
+        final String baseUrl = getBaseUrl() + "/quizz";
+        URI url = new URI(baseUrl);
+        return restTemplate.getForEntity(url, QuizzSession.class);
+    }
 
-    assertDoesNotThrow(() -> restTemplate.delete(url));
-  }
+    @Test
+    public void testGetSession() throws Exception {
+        ResponseEntity<QuizzSession> result = getSession();
+
+        assertEquals(200, result.getStatusCodeValue());
+        assertNotNull(result.getBody());
+        QuizzSession session = result.getBody();
+
+        assertNotNull(session.getId());
+        assertNotNull(session.getCreationTime());
+        assertFalse(session.getId().isEmpty());
+    }
+
+    @Test
+    void testDeleteSessionIllegal() throws URISyntaxException {
+        RestTemplate restTemplate = new RestTemplate();
+        final String baseUrl = getBaseUrl() + "/quizz/abc";
+        URI url = new URI(baseUrl);
+
+        try {
+            restTemplate.delete(url);
+        } catch (RestClientException e) {
+            assertTrue(e.getMessage().startsWith("410"));
+        }
+    }
+
+    @Test()
+    void testDeleteSessionLegal() throws URISyntaxException {
+        ResponseEntity<QuizzSession> result = getSession();
+        String session = result.getBody().getId();
+
+        RestTemplate restTemplate = new RestTemplate();
+        final String baseUrl = getBaseUrl() + "/quizz/" + session;
+        URI url = new URI(baseUrl);
+
+        assertDoesNotThrow(() -> restTemplate.delete(url));
+    }
 
 }
